f8a9b94acd0dae79fc35a54ddf2207ec39e383e0
Rubens Rabelo <rubensrabelo123@gmail.com>
2026-08-23 20:49:53 -0300

    feat(project): initialize spring boot setup with web, jpa, postgresql, flyway and lombok

---
+++ spring-boot/idempotent-payment-system/src/main/java/com/payment/idempotent_payment_system/IdempotentPaymentSystemApplication.java
@@ -0,0 +1,13 @@
+package com.payment.idempotent_payment_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdempotentPaymentSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotentPaymentSystemApplication.class, args);
+	}
+
+}
---
+++ spring-boot/idempotent-payment-system/src/test/java/com/payment/idempotent_payment_system/IdempotentPaymentSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.payment.idempotent_payment_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IdempotentPaymentSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
